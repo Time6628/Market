4f2d28c089da077d2092fdbbe06aca28c02b3d50
Time6628 <pturchan@yahoo.com>
2017-04-11 13:36:10 -0400

    Initial commit

---
+++ src/main/java/com/kookykraftmc/market/commands/subcommands/blacklist/BlacklistRemoveCommand.java
@@ -0,0 +1,29 @@
+package com.kookykraftmc.market.commands.subcommands.blacklist;
+
+import com.kookykraftmc.market.Market;
+import com.kookykraftmc.market.Texts;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+
+import java.util.Optional;
+
+/**
+ * Created by TimeTheCat on 3/26/2017.
+ */
+public class BlacklistRemoveCommand implements CommandExecutor {
+    Market pl = Market.instance;
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        Optional<String> oid = args.getOne("id");
+        if (oid.isPresent()) {
+            String id = oid.get();
+            boolean s = pl.blacklistRemoveCmd(id);
+            if (s) src.sendMessage(Texts.BLACKLIST_REMOVED);
+            else src.sendMessage(Texts.BLACKLIST_REMOVED_FAIL);
+        }
+        return CommandResult.success();
+    }
+}
